429d083b3a42a053d59c1be45142db1ca143859a
Bappi Rahman <cs.bappirahman@gmail.com>
2022-06-19 19:15:01 +0600

    bug fix

--- pages/create-items.jsx
+++ pages/create-items.jsx
@@ -2,7 +2,7 @@ import {useState} from 'react';
 import { ethers } from 'ethers';
 import {create as ipfsHttpClient} from 'ipfs-http-client';
 import { useRouter } from 'next/router';
-import Web3Modal from 'web3Modal';
+import Web3Modal from 'web3modal';
 import { nftAddress, nftMarketAddress } from "../config";
 import NFT from '../artifacts/contracts/NFT.sol/NFT.json';
 import Market from '../artifacts/contracts/NFTMarket.sol/NFTMarket.json';
